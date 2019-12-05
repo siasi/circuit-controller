c2218c6bbe1158f0c3582763bfd6a030b1f51266
Stefano Iasi <siasi@cisco.com>
2019-12-05 11:07:14 +0100

    The controller returns a fixed list of circuits

--- src/main/java/org/siasi/springbootrest/CircuitController.java
+++ src/main/java/org/siasi/springbootrest/CircuitController.java
@@ -3,8 +3,13 @@ package org.siasi.springbootrest;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CircuitController {
 
+    @RequestMapping("/circuits")
     public List<Circuit> getCircuits() {
 	EndPoint aEndPoint = new EndPoint("1.1.1.1", "A");
 	EndPoint zEndPoint = new EndPoint("1.1.1.2", "C");
